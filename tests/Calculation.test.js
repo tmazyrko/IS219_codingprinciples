63acb102047b8da378d30e6992b1f3e6599a9699
tmazyrko <tmazyrko@gmail.com>
2021-02-15 13:36:07 -0500

    Corrected parentheses on each expect statement

--- tests/Calculation.test.js
+++ tests/Calculation.test.js
@@ -18,35 +18,35 @@ test('Create Calculation object', () => {
 test('Test GetResults for Sum', () => {
     let op = Sum;
     let calculation = new Calculation(1, 2, op);
-    expect(calculation.GetResults().toBe(3));
+    expect(calculation.GetResults()).toBe(3);
 })
 
 test('Test GetResults for Difference', () => {
     let op = Difference;
     let calculation = new Calculation(1, 2, op);
-    expect(calculation.GetResults().toBe(-1));
+    expect(calculation.GetResults()).toBe(-1);
 })
 
 test('Test GetResults for Product', () => {
     let op = Product;
     let calculation = new Calculation(1, 2, op);
-    expect(calculation.GetResults().toBe(2));
+    expect(calculation.GetResults()).toBe(2);
 })
 
 test('Test GetResults for Quotient', () => {
     let op = Quotient;
     let calculation = new Calculation(1, 2, op);
-    expect(calculation.GetResults().toBe(0.5));
+    expect(calculation.GetResults()).toBe(0.5);
 })
 
 test('Test GetResults for Power', () => {
     let op = Power;
     let calculation = new Calculation(2, 3, op);
-    expect(calculation.GetResults().toBe(8));
+    expect(calculation.GetResults()).toBe(8);
 })
 
 test('Test GetResults for Root', () => {
     let op = Root;
     let calculation = new Calculation(4, 2, op);
-    expect(calculation.GetResults().toBe(2));
+    expect(calculation.GetResults()).toBe(2);
 })
