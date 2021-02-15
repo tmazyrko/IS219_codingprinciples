8d580d941de5b96f9ce0ccfe1c3b8502c79bb2b7
tmazyrko <tmazyrko@gmail.com>
2021-02-15 13:17:36 -0500

    Added require statements to get operation modules

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,11 @@
-const MathOperations = require('./operations/MathOperations');
+//const MathOperations = require('./operations/MathOperations');
 const Calculation = require('./models/Calculation');
+const Sum = require('./operations/Sum')
+const Difference = require('./operations/Difference')
+const Product = require('./operations/Product')
+const Quotient = require('./operations/Quotient')
+const Power = require('./operations/Power')
+const Root = require('./operations/Root')
 
 class Calculator {
     // Static methods can be called without instantiating, and are good for actions
