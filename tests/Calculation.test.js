3885a0f6702ae99e548457b780daa106027f3a37
tmazyrko <tmazyrko@gmail.com>
2021-02-15 13:33:19 -0500

    Added unit tests for Calculation

--- tests/Calculation.test.js
+++ tests/Calculation.test.js
@@ -1,14 +1,52 @@
 const Calculation = require('../src/models/Calculation');
+const Sum = require('../src/operations/Sum');
+const Difference = require('../src/operations/Difference');
+const Product = require('../src/operations/Sum');
+const Quotient = require('../src/operations/Difference');
+const Power = require('../src/operations/Product');
+const Root = require('../src/operations/Quotient');
 
 test('Create Calculation object', () => {
     // Tests the instantiation of the Calculation object
-    let calculation = new Calculation(1,2,"Sum");
+    let op = Sum;
+    let calculation = new Calculation(1,2,op);
     expect(calculation.a).toBe(1)
     expect(calculation.b).toBe(2)
-    expect(calculation.op).toBe("Sum")
+    expect(calculation.op).toBe(op)
 })
 
-test('Test GetResults function', () => {
-    let calculation = new Calculation(1,2,"Sum");
-    expect(calculation.GetResults()).toBe(3);
+test('Test GetResults for Sum', () => {
+    let op = Sum;
+    let calculation = new Calculation(1, 2, op);
+    expect(calculation.GetResults().toBe(3));
+})
+
+test('Test GetResults for Difference', () => {
+    let op = Difference;
+    let calculation = new Calculation(1, 2, op);
+    expect(calculation.GetResults().toBe(-1));
+})
+
+test('Test GetResults for Product', () => {
+    let op = Product;
+    let calculation = new Calculation(1, 2, op);
+    expect(calculation.GetResults().toBe(2));
+})
+
+test('Test GetResults for Quotient', () => {
+    let op = Quotient;
+    let calculation = new Calculation(1, 2, op);
+    expect(calculation.GetResults().toBe(0.5));
+})
+
+test('Test GetResults for Power', () => {
+    let op = Power;
+    let calculation = new Calculation(2, 3, op);
+    expect(calculation.GetResults().toBe(8));
+})
+
+test('Test GetResults for Root', () => {
+    let op = Root;
+    let calculation = new Calculation(4, 2, op);
+    expect(calculation.GetResults().toBe(2));
 })
