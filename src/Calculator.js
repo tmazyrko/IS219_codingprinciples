901cfa5d99bafbc38cf5668a46dc6d00411ff27a
tmazyrko <tmazyrko@gmail.com>
2021-02-15 13:21:09 -0500

    Added functions for each operations

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,9 +8,38 @@ const Power = require('./operations/Power')
 const Root = require('./operations/Root')
 
 class Calculator {
+    static Calculations = []
+
     // Static methods can be called without instantiating, and are good for actions
     static Sum(a,b) {
-        return new Calculation(a,b,"Sum");
+        let calculation = new Calculation(a, b, Sum);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Difference(a,b) {
+        let calculation = new Calculation(a, b, Difference);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Product(a,b) {
+        let calculation = new Calculation(a, b, Product);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Quotient(a,b) {
+        let calculation = new Calculation(a, b, Quotient);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Power(a,b) {
+        let calculation = new Calculation(a, b, Power);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Root(a,b) {
+        let calculation = new Calculation(a, b, Root);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
     }
 }
 
